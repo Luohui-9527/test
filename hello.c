2aef6604582b58dfea39ffd258e1b9fe801cf193
Luohui-9527 <52158072+Luohui-9527@users.noreply.github.com>
2019-07-21 11:14:48 +0800

    new

--- hello.c
+++ hello.c
@@ -3,9 +3,6 @@
 int main(int arg, char* argv[]){
     printf("inited by SCM.\n");
     printf("develop branch created by SCM.n");
-<<<<<<< HEAD
     printf("dev2");
-=======
     printf("dev1");
->>>>>>> ef5b0fe4ad590a76958f329444bc26fff53620de
 }
